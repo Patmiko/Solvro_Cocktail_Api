5e69a6c8d1a25e67554071996d53da6151a82e68
patmik <patmikdev@gmail.com>
2025-11-02 18:12:02 +0100

    fix: conflicting routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,37 @@
+import path from "node:path";
+
 import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
+import { ServeStaticModule } from "@nestjs/serve-static";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { PrismaModule } from "./prisma/prisma.module";
-import { ServeStaticModule } from "@nestjs/serve-static";
-import { join } from "node:path";
 import { AuthModule } from "./auth/auth.module";
+import { CocktailCategoriesModule } from "./cocktail-categories/cocktail-categories.module";
+import { CocktailIngredientsModule } from "./cocktail-ingredients/cocktail-ingredients.module";
+import { CocktailsModule } from "./cocktails/cocktails.module";
+import { IngredientTypesModule } from "./ingredient-types/ingredient-types.module";
+import { IngredientsModule } from "./ingredients/ingredients.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { RatingsModule } from "./ratings/ratings.module";
 import { UserModule } from "./user/user.module";
-import { ScheduleModule } from '@nestjs/schedule';
-import { RatingsModule } from './ratings/ratings.module';
-import { CocktailIngredientsModule } from './cocktail-ingredients/cocktail-ingredients.module';
-import { CocktailCategoriesModule } from './cocktail-categories/cocktail-categories.module';
-import { CocktailsModule } from './cocktails/cocktails.module';
-import { IngredientTypesModule } from './ingredient-types/ingredient-types.module';
-import { IngredientsModule } from './ingredients/ingredients.module';
-
 
 @Module({
-  imports: [PrismaModule,
+  imports: [
+    PrismaModule,
     ScheduleModule.forRoot(),
     ServeStaticModule.forRoot({
-      rootPath:join(__dirname,"../..","media"),
-      serveRoot:"/media",
+      rootPath: path.join(__dirname, "../..", "media"),
+      serveRoot: "/media",
     }),
     AuthModule,
     UserModule,
-    IngredientsModule,
     IngredientTypesModule,
-    CocktailsModule,
     CocktailCategoriesModule,
     CocktailIngredientsModule,
     RatingsModule,
+    CocktailsModule,
+    IngredientsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/ingredient-types/ingredient-types.controller.ts
+++ src/ingredient-types/ingredient-types.controller.ts
@@ -1,23 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
-import { IngredientTypesService } from './ingredient-types.service';
-import { CreateIngredientTypeDto } from './dto/create-ingredient-type.dto';
-import { UpdateIngredientTypeDto } from './dto/update-ingredient-type.dto';
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { RoleGuard } from '../auth/roles/role.guard';
-import { AuthGuard } from '../auth/auth.guard';
-import { Roles } from '../auth/roles/role.decorator';
-import { Role } from '@prisma/client';
+import { Role } from "@prisma/client";
 
-@Controller('ingredients/types')
-@ApiTags('Ingredient Types')
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from "@nestjs/common";
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from "@nestjs/swagger";
+
+import { AuthGuard } from "../auth/auth.guard";
+import { Roles } from "../auth/roles/role.decorator";
+import { RoleGuard } from "../auth/roles/role.guard";
+import { CreateIngredientTypeDto } from "./dto/create-ingredient-type.dto";
+import { UpdateIngredientTypeDto } from "./dto/update-ingredient-type.dto";
+import { IngredientTypesService } from "./ingredient-types.service";
+
+@Controller("ingredients/types")
+@ApiTags("Ingredient Types")
 export class IngredientTypesController {
-  constructor(private readonly ingredientTypesService: IngredientTypesService) {}
+  constructor(
+    private readonly ingredientTypesService: IngredientTypesService,
+  ) {}
 
   @Post()
-  @ApiOperation({ summary: 'Create a new ingredient type' })
-  @ApiResponse({ status: 201, description: 'Ingredient type successfully created.' })
-  @ApiResponse({ status: 400, description: 'Invalid input data.' })
-  @UseGuards(AuthGuard,RoleGuard)
+  @ApiOperation({ summary: "Create a new ingredient type" })
+  @ApiResponse({
+    status: 201,
+    description: "Ingredient type successfully created.",
+  })
+  @ApiResponse({ status: 400, description: "Invalid input data." })
+  @UseGuards(AuthGuard, RoleGuard)
   @Roles(Role.ADMIN, Role.MODERATOR)
   @ApiBearerAuth("access-token")
   async create(@Body() createIngredientTypeDto: CreateIngredientTypeDto) {
@@ -25,39 +46,54 @@ export class IngredientTypesController {
   }
 
   @Get()
-  @ApiOperation({ summary: 'Retrieve all ingredient types' })
-  @ApiResponse({ status: 200, description: 'List of all ingredient types returned.' })
+  @ApiOperation({ summary: "Retrieve all ingredient types" })
+  @ApiResponse({
+    status: 200,
+    description: "List of all ingredient types returned.",
+  })
   async findAll() {
     return await this.ingredientTypesService.findAll();
   }
 
-  @Get(':name')
-  @ApiOperation({ summary: 'Get a specific ingredient type by ID' })
-  @ApiResponse({ status: 200, description: 'Ingredient type found.' })
-  @ApiResponse({ status: 404, description: 'Ingredient type not found.' })
-  async findOne(@Param('name') name: string) {
+  @Get(":name")
+  @ApiOperation({ summary: "Get a specific ingredient type by Name" })
+  @ApiResponse({ status: 200, description: "Ingredient type found." })
+  @ApiResponse({ status: 404, description: "Ingredient type not found." })
+  async findOne(@Param("name") name: string) {
     return await this.ingredientTypesService.findOne(name);
   }
 
-  @Patch(':name')
-  @ApiOperation({ summary: 'Update an existing ingredient type' })
-  @ApiResponse({ status: 200, description: 'Ingredient type successfully updated.' })
-  @ApiResponse({ status: 404, description: 'Ingredient type not found.' })
-  @UseGuards(AuthGuard,RoleGuard)
+  @Patch(":name")
+  @ApiOperation({ summary: "Update an existing ingredient type" })
+  @ApiResponse({
+    status: 200,
+    description: "Ingredient type successfully updated.",
+  })
+  @ApiResponse({ status: 404, description: "Ingredient type not found." })
+  @UseGuards(AuthGuard, RoleGuard)
   @Roles(Role.ADMIN, Role.MODERATOR)
   @ApiBearerAuth("access-token")
-  async update(@Param('name') name: string, @Body() updateIngredientTypeDto: UpdateIngredientTypeDto) {
-    return await this.ingredientTypesService.update(name, updateIngredientTypeDto);
+  async update(
+    @Param("name") name: string,
+    @Body() updateIngredientTypeDto: UpdateIngredientTypeDto,
+  ) {
+    return await this.ingredientTypesService.update(
+      name,
+      updateIngredientTypeDto,
+    );
   }
 
-  @Delete(':name')
-  @ApiOperation({ summary: 'Delete an ingredient type by ID' })
-  @ApiResponse({ status: 200, description: 'Ingredient type successfully deleted.' })
-  @ApiResponse({ status: 404, description: 'Ingredient type not found.' })
-  @UseGuards(AuthGuard,RoleGuard)
+  @Delete(":name")
+  @ApiOperation({ summary: "Delete an ingredient type by Name" })
+  @ApiResponse({
+    status: 200,
+    description: "Ingredient type successfully deleted.",
+  })
+  @ApiResponse({ status: 404, description: "Ingredient type not found." })
+  @UseGuards(AuthGuard, RoleGuard)
   @Roles(Role.ADMIN, Role.MODERATOR)
   @ApiBearerAuth("access-token")
-  async remove(@Param('name') name: string) {
+  async remove(@Param("name") name: string) {
     return await this.ingredientTypesService.remove(name);
   }
 }
